032ae330cf8d5cc5995fad78b151ed94d82b33ee
ali aziz <aa_connect@outlook.com>
2018-07-09 13:24:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
+  //katzDeliLine = ["Ali"]
+  //name = "Steven"
   katzDeliLine.push(name)
+  //katzDeliLine = ["Ali", "Steven"]
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine) {
+  //katzDeliLine = ["Ali", "Steven"]
+  //katzDeliLine.length = 2
+  //!katzDeliLine.length = false
   if (!katzDeliLine.length){
     return "There is nobody waiting to be served!"
   }
+  
   return `Currently serving ${katzDeliLine.shift()}.`
+
+  //katzDeliLine = ["steve"]
 }
 
 function currentLine(katzDeliLine) {
+  //katzDeliLine = ["Ali", "Steven"]
   if (!katzDeliLine.length){
     return "The line is currently empty."
   }
